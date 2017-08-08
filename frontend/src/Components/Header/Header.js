2db574c2e3a2d62b77381bd2d356707aa378a63b
Wiz1221 <geokyan89@gmail.com>
2017-08-08 14:55:50 +0800

    ported over

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {connect} from 'react-redux';
+import { getUser } from '../../Actions/User';
+import './Header.css';
+import logo from './logo.png';
+import {
+  Link
+} from 'react-router-dom';
+import { store } from '../../index';
+import { push } from 'react-router-redux';
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: ""
+    }
+  }
+
+  logout = (e) => {
+    e.preventDefault();
+    axios.get('/auth/logout')
+    .then( (response) => {
+      console.log("AJAX: Logged out @ '/auth/logout'");
+      store.dispatch(push('/'));
+      this.props.getUser();
+      // window.location.href = "/";
+    })
+    .catch((error)=> {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="row header">
+        <div className="col-sm-2" id="logo">
+          <Link to="/">
+            <img src={logo} alt="logo"/>
+          </Link>
+        </div>
+        <div className="col-sm-7" id="headerSearchBar">
+          {/*<div className="input-group stylish-input-group">
+            <input type="text" className="form-control"  placeholder="Search" />
+            <span className="input-group-addon">
+
+                <button type="submit" onClick=this.onClick>
+                    <span className="glyphicon glyphicon-search"></span>
+                </button>
+            </span>
+        </div>*/}
+        </div>
+
+        <div className="col-sm-4">
+          { this.props.user.email === undefined ?
+            <Link to="/login"><div className="header-button login-button">Login</div></Link> :
+            ( <div>
+                <div className="col-sm-4">
+                  <div className="header-button user-name-display">{this.props.user.email}</div>
+                </div>
+                <div className="col-sm-4">
+                  <Link to="/account"><div className="header-button account-button">Your Profile</div></Link>
+                </div>
+                <div className="col-sm-4">
+                  <a onClick={this.logout}><div className="header-button logout-button">Logout</div></a>
+                </div>
+              </div>
+            )
+          }
+        </div>
+
+      </div>
+
+    );
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getUser:() => {dispatch(getUser());}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { getRestaurant } from './Actions/Admin/Restaurant';
+import { getUser} from './Actions/User';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { Provider } from 'react-redux';   // yarn add react-redux
+import { initStore } from './Store/Store';  //calling initStore function from Store file, already have createStore inside
+//API
+// import {setAdmin, getAdmin} from './API/generalAPI';
+
+export const store = initStore();
+
+// store.subscribe( () => {
+//   const state = store.getState();
+//   setAdmin(state.admin);
+// })
+
+store.dispatch(getRestaurant());
+store.dispatch(getUser());
+
+// PUT GET USER HERE
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
