bcf6cd6d45a4b27fefba84b5175e8d3e28f88353
Wiz1221 <geokyan89@gmail.com>
2017-08-08 16:15:09 +0800

    moved admin page to account page and dhea@dhea.com (pwd=dhea) is the only one access to admin

--- frontend/src/Components/Admin/Admin.js
+++ frontend/src/Components/Admin/Admin.js
@@ -2,25 +2,14 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Compose from './Compose/Compose';
 import RestaurantList from './RestaurantList/RestaurantList';
-import Header from '../Header/Header';
 
 import './Admin.css';
 
 class Admin extends Component {
 
-  // componentWillMount(){
-  //   if(this.props.user.email === ("gy89@gmail.com"||"dhea@dhea.com"||"bryan@gmail.com")){
-  //     console.log(this.props.user)
-  //   }else{
-  //     console.log("else",this.props.user)
-  //     window.location.href='/';
-  //   }
-  // }
-
   render() {
     return (
       <div>
-        <Header/>
         <div className="App container">
           <div className="row todo">
             <div className="col-md-3" id="RestaurantList">
@@ -43,10 +32,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-
-  }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Admin);
+export default connect(mapStateToProps)(Admin);
